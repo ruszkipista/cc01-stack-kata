8c8f5f0fc4be454660054563ad53dcaf89ae8d32
ruszkipista <oi5672@gmail.com>
2021-06-03 19:00:52 +0100

    add testcase afterOnePush_SizeShouldBeOne

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,7 +1,17 @@
 package stack;
 
 public class Stack {
+    private int size;
+
     public boolean isEmpty() {
         return true;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void push(int i) {
+        size++;
     }
 }
--- src/stack/StackTest.java
+++ src/stack/StackTest.java
@@ -1,13 +1,24 @@
 package stack;
 
 import org.junit.Test;
+import org.junit.Assert;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class StackTest {
     @Test
-    public void createStack() throws Exception {
+    public void newlyCreatedStack_ShouldBeEmpty() throws Exception {
         Stack stack = new Stack();
         assertTrue(stack.isEmpty());
+        assertEquals(0,stack.getSize());
     }
+    
+    @Test
+    public void afterOnePush_SizeShouldBeOne() throws Exception {
+        Stack stack = new Stack();
+        stack.push(1);
+        assertEquals(1,stack.getSize());
+    }
+    
 }
